d3186690606c16e2f9fc958ca13921a388758614
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-09-02 12:22:18 +0200

    Add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/quote',
+      text: 'Quote',
+    },
+    {
+      id: 3,
+      path: '/calc',
+      text: 'Calculator',
+    },
+  ];
+  return (
+    <nav className="navBar">
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink to={link.path} activeClassName="active-link" exact>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;

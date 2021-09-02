117368da39d9d208e7d9cd60f4297fceb2d5d03f
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-09-02 14:15:46 +0200

    Add a p

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,9 +6,12 @@ export default function Calculator(props) {
   // eslint-disable-next-line react/prop-types
   const { result, handleClick } = props;
   return (
-    <div className="calc-container">
-      <Result result={result} />
-      <Buttons handleClick={handleClick} />
-    </div>
+    <>
+      <p id="calc-p"> Let&lsquo;s do some math! </p>
+      <div className="calc-container">
+        <Result result={result} />
+        <Buttons handleClick={handleClick} />
+      </div>
+    </>
   );
 }
